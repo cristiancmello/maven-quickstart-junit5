00d2eabc2bc620ba256c78b0d679fc4ef52e891c
Cristian Mello <cristianc.mello@gmail.com>
2023-08-31 21:27:18 -0300

    Refactoring package name

--- src/main/java/org/quickstart/Main.java
+++ src/main/java/org/quickstart/Main.java
@@ -1,14 +1,7 @@
-package org.example;
+package org.quickstart;
 
 public class Main {
-  private Main() {
-  }
-
   public static void main(String[] args) {
     System.out.println("Hello world!");
-  }
-
-  public static int sum(int a, int b) {
-    return a + b;
   }
 }
