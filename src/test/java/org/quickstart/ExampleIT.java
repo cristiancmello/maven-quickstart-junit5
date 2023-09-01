ba385d602d79f754179fb75d2ce9c1b1459752df
Cristian Mello <cristianc.mello@gmail.com>
2023-08-31 21:29:23 -0300

    Refactoring ExampleTest and IT

---
+++ src/test/java/org/quickstart/ExampleIT.java
@@ -0,0 +1,9 @@
+package org.quickstart;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleIT {
+  @Test
+  public void nothing() {
+  }
+}
---
+++ src/test/java/org/quickstart/ExampleTest.java
@@ -0,0 +1,9 @@
+package org.quickstart;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+  @Test
+  public void nothing() {
+  }
+}
